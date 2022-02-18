76e3a9bb09e1a6b297e32a0bde602d13240fb0dc
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-02-18 13:03:52 +0100

    update lua lsp

--- nvim/.config/nvim/lua/jmbuhrconfig/lsp.lua
+++ nvim/.config/nvim/lua/jmbuhrconfig/lsp.lua
@@ -140,22 +140,15 @@ require('lspconfig').sumneko_lua.setup {
   settings = {
     Lua = {
       runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
-        -- Setup your lua path
         path = runtime_path,
       },
       diagnostics = {
-        -- Get the language server to recognize the `vim` global
         globals = { 'vim' },
       },
       workspace = {
-        -- Make the server aware of Neovim runtime files
         library = vim.api.nvim_get_runtime_file('', true),
-        maxPreload = 2000,
-        preloadFileSize = 1000,
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
         enable = false,
       },
