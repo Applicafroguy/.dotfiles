176def6b66946d7b5b123ccf8399b627378c50b3
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-03-09 11:06:06 +0100

    configure nordic

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,11 @@
+require('io')
+require('os')
 
 require('plugins')
 require('autocmd')
 require('globals')
 require('jmbuhrconfig')
 
-vim.cmd[[ colorscheme nord ]]
+vim.cmd"colorscheme nord"
 
 require('colors')
-
-
