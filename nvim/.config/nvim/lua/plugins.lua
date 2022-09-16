8df5ef2be27e011672c4ce27ef4536a0ff9435ea
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-09-16 13:10:24 +0200

    add git blame in statusline

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -211,9 +211,13 @@ require('packer').startup {
         })
       end
     }
+    use {'f-person/git-blame.nvim'}
     use {
       'nvim-lualine/lualine.nvim',
       config = function ()
+        vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+        local git_blame = require('gitblame')
+
         require('lualine').setup{
           options = { section_separators = '', component_separators = '' },
           tabline = {
@@ -224,6 +228,11 @@ require('packer').startup {
             lualine_y = {},
             lualine_z = {{'tabs', mode = 0}}
           },
+          sections = {
+            lualine_c = {
+                { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+            }
+          }
           -- winbar = {
           --   lualine_a = {},
           --   lualine_b = {},
