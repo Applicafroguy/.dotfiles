607fe31dd20d9d94d78cbfdf295e940f1e62cf0c
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-12-02 15:27:50 +0100

    add query to jump to next python code chunk

--- nvim/.config/nvim/lua/plugin-config/treesitter-config.lua
+++ nvim/.config/nvim/lua/plugin-config/treesitter-config.lua
@@ -22,7 +22,8 @@ require 'nvim-treesitter.configs'.setup {
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
         [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
+        -- [']]'] = '@class.outer',
+        [']]'] = '@codechunk',
       },
       goto_next_end = {
         [']M'] = '@function.outer',
@@ -30,7 +31,8 @@ require 'nvim-treesitter.configs'.setup {
       },
       goto_previous_start = {
         ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
+        -- ['[['] = '@class.outer',
+        ['[['] = '@codechunk',
       },
       goto_previous_end = {
         ['[M'] = '@function.outer',
