7cbfd5d7b7b0c1eaa9b9c9f817a4971ac625577e
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-01-16 10:34:27 +0100

    align on binary operators in R

---
+++ nvim/.config/nvim/queries/r/indents.scm
@@ -0,0 +1,33 @@
+[
+  (brace_list)
+  (paren_list)
+  (special)
+  (pipe)
+  (call)
+  (binary)
+  "|>"
+  "if"
+  "else"
+  "while"
+  "repeat"
+  "for"
+] @indent
+
+((binary operator: (special)) @indent)
+
+[
+  "}"
+  ")"
+] @branch
+
+((formal_parameters (identifier)) @aligned_indent
+ (#set! "delimiter" "()"))
+
+[
+  ")"
+  "}"
+] @indent_end
+
+[
+  (comment)
+] @ignore
